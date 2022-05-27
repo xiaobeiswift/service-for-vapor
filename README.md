3640f2ac70b4fe51d7f2139edf3d77e0a3807384
不明不惑 <cit117@126.com>
2022-05-27 22:22:46 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# service-for-vapor
+
+A description of this package.
---
+++ Sources/service-for-vapor/service_for_vapor.swift
@@ -0,0 +1,6 @@
+public struct service_for_vapor {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
